adfac139b206e9437dd1683c1a92643a57f6e3e0
ZIGCER <96gara@gmail.com>
2021-09-28 17:23:28 +0700

    race

--- main.go
+++ main.go
@@ -5,22 +5,24 @@ import (
 	"sync"
 )
 
-var chDeposit = make(chan int)
+var chDeposit = make(chan int, 1000)
 var chBalance = make(chan int)
 
 func main() {
 
 	go bankSystem()
+	fmt.Println("Balance is : ", finalBalance())
 
 	w := &sync.WaitGroup{}
 
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 100000; i++ {
 
 		w.Add(1)
 
 		go deposit(100, w)
 
 	}
+	fmt.Println("Balance is : ", finalBalance())
 
 	w.Wait()
 
@@ -45,11 +47,17 @@ func bankSystem() {
 	balance := 0
 
 	for {
-		select {
-		case x := <-chDeposit:
-			balance += x
+		if len(chDeposit) == 0 {
+			select {
+			case x := <-chDeposit:
+				balance += x
 
-		case chBalance <- balance:
+			case chBalance <- balance:
+
+			}
+		} else {
+			x := <-chDeposit
+			balance += x
 
 		}
 
